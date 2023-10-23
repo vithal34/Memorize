ddfa40713b44789b8503c21fbf8c26164360c209
Vithal Agrawal <vithalagrawal1@gmail.com>
2023-10-23 11:06:21 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Vithal Agrawal on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
